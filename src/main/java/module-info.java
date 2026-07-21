6ab0b6fd869df713430155201b953b40d580c80f
Velomega <Alejandrovalenciasandoval@gmail.com>
2026-07-21 12:04:51 -0500

    modulo arreglado

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
 
-
+    opens com.example.miniproyecto4.controller to javafx.fxml;
+    opens com.example.miniproyecto4.Views to javafx.fxml;
     opens com.example.miniproyecto4 to javafx.fxml;
     exports com.example.miniproyecto4;
 }
