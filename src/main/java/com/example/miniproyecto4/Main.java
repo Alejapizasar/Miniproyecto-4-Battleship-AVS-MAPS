c58adc3ed2c98288bee26c06010d8f78992c574a
Maria Alejandra Pizarro Sarria <alejapizasar@gmail.com>
2026-07-23 00:16:22 -0500

    fix(ui): fix truncated button text and add app icon

--- src/main/java/com/example/miniproyecto4/Main.java
+++ src/main/java/com/example/miniproyecto4/Main.java
@@ -4,12 +4,14 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
     private static final String HOME_VIEW_FXML = "/com/example/miniproyecto4/Views/HomeView.fxml";
+    private static final String APP_ICON = "/com/example/miniproyecto4/Iconos/logo.png";
 
     @Override
     public void start(Stage primaryStage) throws IOException
@@ -17,6 +19,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource(HOME_VIEW_FXML));
         Scene scene = new Scene(root);
 
+        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream(APP_ICON)));
         primaryStage.setTitle("Battleship - Naval Command");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
